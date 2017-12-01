2426bb6df752ca0677b1c063e85222a4e4eec4c9
mostafaesmaeili <mostafa.esmaeili8@gmail.com>
2017-12-02 00:45:39 +0330

    Search for job

---
+++ App/Src/IranMarketer.App/Controllers/SearchForJobController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IranMarketer.App.Controllers
+{
+    public class SearchForJobController : Controller
+    {
+        // GET: SearchForJob
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
